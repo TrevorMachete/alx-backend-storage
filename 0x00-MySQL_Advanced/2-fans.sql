d270c0f584f7ef082f41dc345b690a96fb7859c3
Macs Machete <trevormachete@gmail.com>
2024-06-13 17:09:04 +0200

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,15 +1,5 @@
--- Task: Rank country origins of bands, ordered by the number of (non-unique) fans
-CREATE TABLE IF NOT EXISTS bands (
-    -- 'id' is an integer, never null, auto increment and primary key
-    id INT AUTO_INCREMENT,
-    -- 'origin' is an enumeration of countries: US, CO and TN, never null
-    origin ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-    -- 'fans' is an integer
-    fans INT,
-    PRIMARY KEY(id)
-);
-
-SELECT origin, COUNT(fans) AS nb_fans
-FROM bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+-- SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
