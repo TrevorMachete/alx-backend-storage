6174efda6b0e658786373faba387fcc3d6c1bffc
Macs Machete <trevormachete@gmail.com>
2024-06-13 17:10:55 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,19 +1,6 @@
--- Task: List all bands with Glam rock as their main style, ranked by their longevity
-CREATE TABLE IF NOT EXISTS bands (
-    -- 'id' is an integer, never null, auto increment and primary key
-    id INT AUTO_INCREMENT,
-    -- 'band_name' is a string (255 characters)
-    band_name VARCHAR(255),
-    -- 'style' is an enumeration of styles
-    style ENUM('Glam rock', 'Other styles'),
-    -- 'formed' is an integer
-    formed INT,
-    -- 'split' is an integer
-    split INT,
-    PRIMARY KEY(id)
-);
-
-SELECT band_name, (2022 - formed) AS lifespan
-FROM bands
-WHERE style = 'Glam rock' AND split IS NOT NULL
-ORDER BY lifespan DESC;
+-- SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2020) - `formed` as `lifespan`
+  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+  ORDER BY `lifespan` DESC;
